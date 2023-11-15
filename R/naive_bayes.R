7c52ea953e7908c1cc2fbfb3e4845381f411998e
moiseberthe <berthemoise890@gmail.com>
2023-11-15 15:08:43 +0100

    add metrics and rename classifier

---
+++ R/naive_bayes.R
@@ -0,0 +1,78 @@
+#' naive_bayes
+#'
+#' This class implements a supervised learning methods based on applying Bayes’
+#' theorem with strong (naive) feature independence assumption
+#' @importFrom R6 R6Class
+#'
+#' @export
+naive_bayes <- R6Class("naive_bayes",
+  public = list(
+    n_samples = NA,
+    n_features = NA,
+    classes = NA,
+    mean_ = NA,
+    var_ = NA,
+    prior_ = NA,
+
+    fit = function(x, y) {
+      d <- dim(x)
+      self$n_samples <- d[1]
+      self$n_features <- d[2]
+      # Les différentes classes
+      self$classes <- unique(y)
+      # Le nombre de classes
+      n_classes <- length(self$classes)
+
+      self$mean_ <- t(sapply(
+        self$classes,
+        function(class_) colMeans(x[y == class_, ])
+      ))
+      self$var_ <- t(sapply(
+        self$classes,
+        function(class_) apply(x[y == class_, ], 2, var)
+      ))
+      self$prior_ <- sapply(
+        self$classes,
+        function(class_) sum(y == class_) / self$n_samples
+      )
+    },
+    prob = function(label, x) {
+      mean <- self$mean_[label, ]
+      var <- self$var_[label, ]
+      numerator <- exp((-(x - mean)**2) / (2 * var))
+      denominator <- sqrt(2 * pi * var)
+      return(numerator / denominator)
+    },
+    predict_ = function(x, with_prob = FALSE) {
+      posteriors <- numeric(length(self$classes))
+
+      for (idx in seq_along(self$classes)) {
+        prior <- log(self$prior_[idx])
+
+        class_conditional <- sum(log(self$prob(idx, x)))
+
+        posterior <- prior + class_conditional
+        posteriors[idx] <- posterior
+      }
+      if (with_prob) {
+        return(softmax(posteriors))
+      } else {
+        return(self$classes[which.max(posteriors)])
+      }
+    },
+    predict = function(x) {
+      ypred <- apply(x, 1, function(row) self$predict_(row))
+      return(ypred)
+    },
+    predict_proba = function(x) {
+      ypred <- apply(x, 1, function(row) self$predict_(row, with_prob = TRUE))
+      return(ypred)
+    },
+    print = function(...) {
+      print("Moyenne : ")
+      print(self$mean_)
+      print("Ecart-type : ")
+      print(self$var_)
+    }
+  )
+)
--- R/train_test_split.R
+++ R/train_test_split.R
@@ -1,7 +1,8 @@
 #' train_test_split
 #'
 #' @param data a data.frame
-#' @param train_size a number between 0 and 1 portion of number to include in train set
+#' @param train_size a number between 0 and 1 portion of number
+#' to include in train set
 #' @param stratify a character the name of column to be use to strafied data
 #' @param seed a number to fixed sample generator
 #'
@@ -10,28 +11,36 @@
 #'
 #' @examples
 #' train_test_split(iris, train_size = 0.7, stratify = "Species", seed = 123)
-train_test_split = function(data, train_size = 0.7, stratify = NULL, seed = NULL) {
+train_test_split <- function(
+  data,
+  train_size = 0.7,
+  stratify = NULL,
+  seed = NULL
+) {
   if (!is.null(seed)) set.seed(seed)
 
   # check if train_size in ]0;1[
-  if(train_size > 1 || train_size < 0){
+  if (train_size > 1 || train_size < 0) {
     stop("The train size must be a float between 0.0 and 1.0")
   }
 
   # check if stratity options is set and exist
   if (!is.null(stratify)) {
-    if(is.null(data[[stratify]])){
+    if (is.null(data[[stratify]])) {
       stop("The stratyfy column doesn't exist")
     }
-    stratified_index = split_indices_stratified(data[[stratify]], train_size)
-    train_index = unlist(stratified_index$train)
+    stratified_index <- split_indices_stratified(data[[stratify]], train_size)
+    train_index <- unlist(stratified_index$train)
   } else {
-    train_index = sample(1:nrow(data), size = floor(train_size * nrow(data)))
+    train_index <- sample(
+      seq_len(nrow(iris)),
+      size = floor(train_size * nrow(data))
+    )
   }
 
   # split data
-  train_set = data[train_index, ]
-  test_set = data[-train_index, ]
+  train_set <- data[train_index, ]
+  test_set <- data[-train_index, ]
 
   return(list(train_set = train_set, test_set = test_set))
 }
